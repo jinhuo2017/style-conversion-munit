faf99afac35ea485fd3e52a39086c0ec0acb4ffe
jinhuo.yang <jinhuo.yang@bitmain.com>
2024-02-22 02:00:05 +0800

    add: 图片展示接口

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, send_from_directory, abort
+from werkzeug.exceptions import NotFound
+
 from server.predict import predict
 from util import generate_image_id, allowed_file
 from werkzeug.utils import secure_filename
@@ -65,6 +67,38 @@ def convert():
         print(f"{input}转换完成")
         return jsonify({'code': 1, 'msg': 'success', 'data': {'processed': processed}})
     return jsonify({'code': 0, 'msg': 'failed'})
+
+
+# 图片展示
+@app.route('/show', methods=['GET'])
+def show_image():
+    username = request.args.get('username')
+    # img_type 0: 原始图 1: 处理后的图像
+    img_type = request.args.get('type')
+    filename = request.args.get('filename')
+    try:
+        # 安全检查：确保没有使用 '..' 来访问文件夹路径之外的文件
+        if '..' in filename or filename.startswith('/') or '../' in username or username.startswith('/'):
+            raise ValueError("无效的username或filename")
+
+        if img_type == '0':
+            relative_dir = upload_relative_dir
+        elif img_type == '1':
+            relative_dir = processed_relative_dir
+        else:
+            raise ValueError('type类型为空')
+
+        file_dir = os.path.join(base_dir, relative_dir, username)
+
+        if not os.path.isdir(file_dir):
+            raise NotFound(f"Image directory {file_dir} not found")
+
+        # send_from_directory 安全发送位于指定文件夹中的文件
+        return send_from_directory(file_dir, filename)
+    except Exception as e:
+        # 捕捉到任何发送过程中的异常，返回500内部服务器错误
+        app.logger.error(f"Error while sending file {filename}: {str(e)}")
+        return "An error occurred while trying to send the file.", 500
 
 
 if __name__ == '__main__':
