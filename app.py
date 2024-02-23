877cb9e112e59ac5b55c3546fd2fd76450028702
winghann <148356582+winghann@users.noreply.github.com>
2024-02-23 12:50:09 +0800

    添加了三个批量处理的接口

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ processed_relative_dir = 'results/processed/'
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!1'
+    return 'Hello World!'
 
 
 # 图片上传接口
@@ -50,6 +50,7 @@ def upload():
         return jsonify({'code': 1, 'msg': 'success', 'data': {'img_name': img_name}})
     else:
         return jsonify({'code': 0, 'msg': '文件上传失败'})
+
 
 
 # 风格迁移接口
@@ -99,6 +100,107 @@ def show_image():
         # 捕捉到任何发送过程中的异常，返回500内部服务器错误
         app.logger.error(f"Error while sending file {filename}: {str(e)}")
         return "An error occurred while trying to send the file.", 500
+
+
+
+##############################################################################winggggggggggggggg
+from PIL import Image
+import io
+import base64
+
+#图片批量上传接口
+@app.route('/multi-upload', methods=['POST'])
+def multi_upload():
+    # 获取用户名
+    username = request.form.get("username")
+    print("base_dir", base_dir)
+    upload_dir = os.path.join(base_dir, upload_relative_dir + username + "/")
+    processed_dir = os.path.join(base_dir, processed_relative_dir + username + "/")
+    if not os.path.exists(upload_dir):
+        print("creating upload_dir: ", upload_dir)
+        os.makedirs(upload_dir)
+    if not os.path.exists(processed_dir):
+        print("creating processed_dir: ", processed_dir)
+        os.makedirs(processed_dir)
+    filename_list=[]#用于存储所有更名后的文件名
+    for file in request.files.getlist('img_name'):
+        if file and allowed_file(file.filename):
+            cur_filename = secure_filename(file.filename)
+            # 获取文件后缀
+            _, ext = os.path.splitext(cur_filename)
+            # 移除点号
+            ext = ext[1:]
+
+            file_name = generate_image_id() + '.' + ext
+            file.save(os.path.join(upload_dir, file_name))
+            filename_list.append(file_name)
+        #     return jsonify({'code': 1, 'msg': 'success', 'data': {'img_name': file_name}})
+        # else:
+        #     return jsonify({'code': 0, 'msg': '文件上传失败'})
+    return jsonify({'code': 1, 'msg': 'success', 'data': {'img_name': filename_list}})
+
+#批量风格迁移接口
+@app.route('/multi-convert', methods=['GET', 'POST'])
+# 传参：给定的图片路径、任务名称
+def multi_convert():
+    if request.method == 'POST':
+        # input: xxx.png eg: 0300.png
+        input_list = request.form.getlist('input')
+        # username: username eg: jinhuoyang
+        username = request.form.get('username')
+        task = request.form.get('task')
+        # processed = predict_batch(input_list, task, username)#！！！！！！！！！！！等待predict_batch函数开发好之后改用此行！！！！！！！！！！！
+        processed = predict(input_list[1], task, username)#！！！！！！！！！！！！注意，当predict_batch函数开发好之后将此行注释掉，改用上一行！！！！！！！！！！！
+
+        print(f"{input}转换完成")
+        return jsonify({'code': 1, 'msg': 'success', 'data': {'processed': input_list}})
+    return jsonify({'code': 0, 'msg': 'failed'})
+
+#批量展示接口
+@app.route('/multi-show', methods=['GET'])
+def multi_show_image():
+    username = request.args.get('username')
+    # img_type 0: 原始图 1: 处理后的图像
+    img_type = request.args.get('type')
+    filename_list = request.args.getlist('filename')  # 将存储所有要展示的图片的名字，使用列表进行存储
+    try:
+        # 安全检查：确保没有使用 '..' 来访问文件夹路径之外的文件
+        if '../' in username or username.startswith('/'):
+            return jsonify({'code': 0, 'msg': '无效的username'})
+        for filename in filename_list:
+            if filename.startswith('/') or '..' in filename:
+                return jsonify({'code': 0, 'msg': '无效的filename'})
+
+        if img_type == '0':
+            relative_dir = upload_relative_dir
+        elif img_type == '1':
+            relative_dir = processed_relative_dir
+        else:
+            return jsonify({'code': 0, 'msg': '无效的type'})
+
+        file_dir = os.path.join(base_dir, relative_dir, username)
+
+        if not os.path.isdir(file_dir):
+            return jsonify({'code': 0, 'msg': f"Image directory {file_dir} not found"})
+
+        image_data = []
+        for filename in filename_list:
+            filepath = os.path.join(file_dir, filename)
+
+            if os.path.isfile(filepath):
+                with Image.open(filepath) as img:
+                    byte_arr = io.BytesIO()
+                    img.save(byte_arr, format='PNG')########！！！！！！！！！此处仅支持png格式的图片，在编解码时均认为图片格式为png！！！！！！！！！！！
+                    encoded_image = base64.encodebytes(byte_arr.getvalue()).decode('ascii')
+                    image_data.append(encoded_image)
+        return jsonify({'code': 1, 'msg': 'success', 'data': {'images': image_data}})
+    except Exception as e:
+        # 捕捉到任何发送过程中的异常，返回500内部服务器错误
+        app.logger.error(f"Error while sending file {filename_list}: {str(e)}")
+        return "An error occurred while trying to send the file.", 500
+###################################################################winggggggggggggggg
+
+
 
 
 if __name__ == '__main__':
